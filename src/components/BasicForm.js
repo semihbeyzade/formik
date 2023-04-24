a78e4f08e990f14284722a53e8c7d2b888f6d4ad
semihbeyzade <semihb.orscelik0@gmail.com>
2023-04-24 14:48:33 +0200

    created basicSchema

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,8 +1,52 @@
+import { useFormik } from "formik";
+import basicSchema from "../schemas";
+
 const BasicForm = () => {
+  const { values, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      email: "",
+      age: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: basicSchema,
+  });
+
   return (
-    <form autoComplete="off">
+    <form onSubmit={handleSubmit} autoComplete="off">
       <label htmlFor="email">Email</label>
-      <input id="email" type="email" placeholder="Enter your email" />
+      <input
+       value={values.email}
+       onChange={handleChange}
+       onBlur={handleBlur}
+       id="email" 
+       type="email" 
+       placeholder="Enter your email" />
+       <label htmlFor="age">Age</label>
+      <input
+       value={values.age}
+       onChange={handleChange}
+       onBlur={handleBlur}
+       id="age" 
+       type="number" 
+       placeholder="Enter your age" />
+       <label htmlFor="password">Password</label>
+      <input
+       value={values.password}
+       onChange={handleChange}
+       onBlur={handleBlur}
+       id="password" 
+       type="password" 
+       placeholder="Enter your password" />
+       <label htmlFor="confirmPassword">Confirm Password</label>
+      <input
+       value={values.confirmPassword}
+       onChange={handleChange}
+       onBlur={handleBlur}
+       id="confirmPassword" 
+       type="password" 
+       placeholder="Confirm password" />
+       <button type="submit">Submit</button>
     </form>
   );
 };
