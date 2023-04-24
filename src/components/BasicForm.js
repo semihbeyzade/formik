de9794db8ea6c6b82763b043ec7a495401b6c755
semihbeyzade <semihb.orscelik0@gmail.com>
2023-04-24 15:21:07 +0200

    created error message

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,8 +1,15 @@
 import { useFormik } from "formik";
 import basicSchema from "../schemas";
 
+const onSubmit = async (values, actions) => {
+  console.log(values);
+  console.log(actions);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm();
+}
+
 const BasicForm = () => {
-  const { values, handleBlur, handleChange, handleSubmit } = useFormik({
+  const { values, errors, touched, isSubmitting, handleBlur, handleChange, handleSubmit } = useFormik({
     initialValues: {
       email: "",
       age: "",
@@ -10,7 +17,10 @@ const BasicForm = () => {
       confirmPassword: "",
     },
     validationSchema: basicSchema,
+    onSubmit,
   });
+
+  console.log(errors);
 
   return (
     <form onSubmit={handleSubmit} autoComplete="off">
@@ -21,7 +31,10 @@ const BasicForm = () => {
        onBlur={handleBlur}
        id="email" 
        type="email" 
-       placeholder="Enter your email" />
+       placeholder="Enter your email"
+       className={errors.email && touched.email ? "input-error" : ""}
+       />
+       {errors.email && touched.email && <p className="error">{errors.email}</p>}
        <label htmlFor="age">Age</label>
       <input
        value={values.age}
@@ -29,7 +42,10 @@ const BasicForm = () => {
        onBlur={handleBlur}
        id="age" 
        type="number" 
-       placeholder="Enter your age" />
+       placeholder="Enter your age"
+       className={errors.age && touched.age ? "input-error" : ""}
+       />
+       {errors.age && touched.age && <p className="error">{errors.age}</p>}
        <label htmlFor="password">Password</label>
       <input
        value={values.password}
@@ -37,7 +53,10 @@ const BasicForm = () => {
        onBlur={handleBlur}
        id="password" 
        type="password" 
-       placeholder="Enter your password" />
+       placeholder="Enter your password" 
+       className={errors.password && touched.password ? "input-error" : ""}
+       />
+       {errors.password && touched.password && <p className="error">{errors.password}</p>}
        <label htmlFor="confirmPassword">Confirm Password</label>
       <input
        value={values.confirmPassword}
@@ -45,8 +64,11 @@ const BasicForm = () => {
        onBlur={handleBlur}
        id="confirmPassword" 
        type="password" 
-       placeholder="Confirm password" />
-       <button type="submit">Submit</button>
+       placeholder="Confirm password" 
+       className={errors.confirmPassword && touched.confirmPassword ? "input-error" : ""}
+       />
+       {errors.confirmPassword && touched.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+       <button disabled={isSubmitting} type="submit">Submit</button>
     </form>
   );
 };
