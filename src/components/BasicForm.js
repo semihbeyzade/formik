1f5489fe1d82c9a7598afb7e3272a19cf80dfee3
semihbeyzade <semihb.orscelik0@gmail.com>
2023-04-25 12:58:12 +0200

    finished

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -26,49 +26,49 @@ const BasicForm = () => {
     <form onSubmit={handleSubmit} autoComplete="off">
       <label htmlFor="email">Email</label>
       <input
-       value={values.email}
-       onChange={handleChange}
-       onBlur={handleBlur}
-       id="email" 
-       type="email" 
-       placeholder="Enter your email"
-       className={errors.email && touched.email ? "input-error" : ""}
-       />
-       {errors.email && touched.email && <p className="error">{errors.email}</p>}
-       <label htmlFor="age">Age</label>
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="email"
+        type="email"
+        placeholder="Enter your email"
+        className={errors.email && touched.email ? "input-error" : ""}
+      />
+      {errors.email && touched.email && <p className="error">{errors.email}</p>}
+      <label htmlFor="age">Age</label>
       <input
-       value={values.age}
-       onChange={handleChange}
-       onBlur={handleBlur}
-       id="age" 
-       type="number" 
-       placeholder="Enter your age"
-       className={errors.age && touched.age ? "input-error" : ""}
-       />
-       {errors.age && touched.age && <p className="error">{errors.age}</p>}
-       <label htmlFor="password">Password</label>
+        value={values.age}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="age"
+        type="number"
+        placeholder="Enter your age"
+        className={errors.age && touched.age ? "input-error" : ""}
+      />
+      {errors.age && touched.age && <p className="error">{errors.age}</p>}
+      <label htmlFor="password">Password</label>
       <input
-       value={values.password}
-       onChange={handleChange}
-       onBlur={handleBlur}
-       id="password" 
-       type="password" 
-       placeholder="Enter your password" 
-       className={errors.password && touched.password ? "input-error" : ""}
-       />
-       {errors.password && touched.password && <p className="error">{errors.password}</p>}
-       <label htmlFor="confirmPassword">Confirm Password</label>
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="password"
+        type="password"
+        placeholder="Enter your password"
+        className={errors.password && touched.password ? "input-error" : ""}
+      />
+      {errors.password && touched.password && <p className="error">{errors.password}</p>}
+      <label htmlFor="confirmPassword">Confirm Password</label>
       <input
-       value={values.confirmPassword}
-       onChange={handleChange}
-       onBlur={handleBlur}
-       id="confirmPassword" 
-       type="password" 
-       placeholder="Confirm password" 
-       className={errors.confirmPassword && touched.confirmPassword ? "input-error" : ""}
-       />
-       {errors.confirmPassword && touched.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
-       <button disabled={isSubmitting} type="submit">Submit</button>
+        value={values.confirmPassword}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="confirmPassword"
+        type="password"
+        placeholder="Confirm password"
+        className={errors.confirmPassword && touched.confirmPassword ? "input-error" : ""}
+      />
+      {errors.confirmPassword && touched.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+      <button disabled={isSubmitting} type="submit">Submit</button>
     </form>
   );
 };
--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -2,14 +2,16 @@ import { useField } from "formik"
 
 const CustomInput = ({label, ...props}) => {
     const [field, meta] = useField(props);
-    console.log("field", field);
-    console.log("meta", meta);
+   
     return (
         <>
          <label>{label}</label>
-         <input {...field} {...props}/>
+         <input {...field} {...props}
+         className={meta.touched && meta.error ? "input-error" : ""}
+         />
+         {meta.touched && meta.error && <div className="error">{meta.error}</div>}
         </>
     )
 }
 
-export default CustomInput
+export default CustomInput;
